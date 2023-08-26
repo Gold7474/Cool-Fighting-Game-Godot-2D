826a60f0be4d6a8ba9d4b8e0340bf5b23e2dbca6
Gold7474 <hayden7482@gmail.com>
2023-08-26 12:58:48 -0500

    Menu and Character Tweaks

--- Scenes/BackButton.gd
+++ Scenes/BackButton.gd
@@ -3,13 +3,13 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	grab_focus()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("Menu Back"):
-		get_tree().change_scene_to_file("res://Scenes/Title Screen.tscn")
+	pass
+
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Node2D.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Title Screen.tscn")
